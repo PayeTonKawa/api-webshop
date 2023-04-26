c6b3d002ff02236743f57ef63413488bbd4b4acf
Guillaume Tauzin <guillaumetauzin@icloud.com>
2023-04-26 18:21:25 +0200

    Add GET, PUT, POST, DELETE to product route

--- pages/api/product/[id].ts
+++ pages/api/product/[id].ts
@@ -6,17 +6,26 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const db = client.db("payetonkawa");
     const clientCollection = await db.collection('Produits');
     const {method, query} = req
-
+    const id = query.id;
     return new Promise(() => {
         switch (method) {
             case 'GET':
-                clientCollection.find({}).toArray(function(err, result) {
+                clientCollection.find({id: id}).toArray(function(err, result) {
                     if (err) throw err;
                     res.status(200).json(result);
                 });
                 break
+            case 'PUT':
+                res.status(501);
+                break
+            case 'POST':
+                res.status(501);
+                break
+            case 'DELETE':
+                res.status(501);
+                break
             default:
-                res.setHeader('Allow', ['GET']);
+                res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
                 res.status(405).end(`Method ${method} Not Allowed`);
         }
     })
