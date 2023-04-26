b2364aa85307d12f1f116cee8184ca7e2837f1d9
Guillaume Tauzin <guillaumetauzin@icloud.com>
2023-04-26 17:17:31 +0200

    Add prospects route

---
+++ pages/api/prospects/index.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import clientPromise from "../../../lib/mongodb";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const client = await clientPromise;
+    const db = client.db("payetonkawa");
+    const clientCollection = await db.collection('Clients');
+    const {method, query} = req
+
+    return new Promise(() => {
+        switch (method) {
+            case 'GET':
+                clientCollection.find({orders: {$exists: true, $eq: []}}).toArray(function(err, result) {
+                    if (err) throw err;
+                    res.status(200).json(result);
+                });
+                break
+            default:
+                res.setHeader('Allow', ['GET']);
+                res.status(405).end(`Method ${method} Not Allowed`);
+        }
+    })
+}
