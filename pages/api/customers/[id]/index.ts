0bff9a02f86e70a70fd09880ea199c6c86a5e0e2
Guillaume Tauzin <guillaumetauzin@icloud.com>
2023-04-27 11:27:15 +0200

    Add customers/id route

---
+++ pages/api/customers/[id]/index.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import clientPromise from "../../../../lib/mongodb";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const client = await clientPromise;
+    const db = client.db("payetonkawa");
+    const clientCollection = await db.collection('customers');
+    const {method, query} = req
+    const id = query.id;
+    return new Promise(() => {
+        switch (method) {
+            case 'GET':
+                clientCollection.find({id: id}).toArray(function(err, result) {
+                    if (err) throw err;
+                    res.status(200).json({"code": 200, "data": result[0]});
+                });
+                break
+            case 'PUT':
+                res.status(501).end();
+                break
+            case 'POST':
+                res.status(501).end();
+                break
+            case 'DELETE':
+                res.status(501).end();
+                break
+            default:
+                res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
+                res.status(405).end(`Method ${method} Not Allowed`);
+        }
+    })
+}
