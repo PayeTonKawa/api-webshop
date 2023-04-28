685d368f082978e3dbee2564308cb0a7b855ab16
Guillaume Tauzin <guillaumetauzin@icloud.com>
2023-04-28 16:02:43 +0200

    Change message

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
     const token = process.env.TOKEN
     if (!(request.headers.get("X-AUTH-TOKEN")==token)) {
-        return NextResponse.json({ message: 'Auth required' }, { status: 401 })
+        return NextResponse.json({ message: 'X-AUTH-TOKEN invalid' }, { status: 401 })
     }
 
     return NextResponse.next()
