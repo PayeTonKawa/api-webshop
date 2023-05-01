1db5eb9ddb4b58fd8a2358c6146a0a69e3004236
Guillaume Tauzin <guillaumetauzin@icloud.com>
2023-05-01 17:15:38 +0200

    Add base for tests

---
+++ tests/pages/middleware.test.js
@@ -0,0 +1,7 @@
+//import middleware from '../../middleware';
+
+describe('Middleware', () => {
+    it('TOKEN invalid', async () => {})
+    it('TOKEN empty', async () => {})
+    it('TOKEN valide', async () => {})
+  })
