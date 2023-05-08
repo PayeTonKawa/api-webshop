61391f46a25ebc184fd175f642e31f8df280c2ae
Guillaume Tauzin <guillaumetauzin@icloud.com>
2023-05-08 19:02:09 +0200

    fix jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,4 @@
 echo "BUILD ================================================================="
-docker compose build  
+docker-compose build  
 echo "DEPLOY ==================================================================="
-docker compose up  
-
-#docker stop api_webshop_epsi || true && docker rm -f api_webshop_epsi || true
-#docker run -d --name api_webshop_epsi -p "4001:3000" api_webshop_epsi
+docker-compose -f ./docker-compose.yml --compatibility -p api_webshop_epsi up -d
