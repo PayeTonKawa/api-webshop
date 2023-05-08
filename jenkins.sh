2f7107969f212f71ada2bfafb5257e87a0fcdb7b
Guillaume Tauzin <guillaumetauzin@icloud.com>
2023-05-08 18:55:37 +0200

    Add scaler config

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,7 @@
 echo "BUILD ================================================================="
-docker build -t api_webshop_epsi .
-echo "RUN ==================================================================="
-docker stop api_webshop_epsi || true && docker rm -f api_webshop_epsi || true
-docker run -d --name api_webshop_epsi -p "4001:3000" api_webshop_epsi
+docker compose build  
+echo "DEPLOY ==================================================================="
+docker compose up  
+
+#docker stop api_webshop_epsi || true && docker rm -f api_webshop_epsi || true
+#docker run -d --name api_webshop_epsi -p "4001:3000" api_webshop_epsi
